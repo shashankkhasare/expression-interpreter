a330610c07e92e9a44ee228fe0887b7d97fe8b91
Shashank Khasare <shashank.khasare@gmail.com>
2022-10-12 22:28:38 +0530

    Add no-op REPL.

--- src/main/java/com/expression/interpreters/App.java
+++ src/main/java/com/expression/interpreters/App.java
@@ -1,13 +1,23 @@
 package com.expression.interpreters;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+  public static void main(String[] args) throws IOException {
+    /* Initialize reader. */
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    /* REPL. */
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) {
+        break;
+      }
+      System.out.println(line);
     }
+  }
 }
