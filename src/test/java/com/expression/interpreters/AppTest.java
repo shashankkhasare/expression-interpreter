1f231ba9af4b7a4cdd7bbd5250774d065cd0f12d
Shashank Khasare <shashank.khasare@gmail.com>
2022-10-12 22:12:57 +0530

    Add maven-archetype-quickstart files.

---
+++ src/test/java/com/expression/interpreters/AppTest.java
@@ -0,0 +1,20 @@
+package com.expression.interpreters;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
