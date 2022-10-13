0e9b60a246ff459845edea2e6cc547ef0c6ca7f1
Shashank Khasare <shashank.khasare@gmail.com>
2022-10-13 12:05:10 +0530

    Change visibility of members.

--- src/main/java/com/expression/interpreters/core/Token.java
+++ src/main/java/com/expression/interpreters/core/Token.java
@@ -3,9 +3,9 @@ package com.expression.interpreters.core;
 import com.expression.interpreters.enums.TokenType;
 
 public class Token {
-  final TokenType type;
-  final String lexeme;
-  final Object literal;
+  public final TokenType type;
+  public final String lexeme;
+  public final Object literal;
 
   Token(TokenType type, String lexeme, Object literal) {
     this.type = type;
